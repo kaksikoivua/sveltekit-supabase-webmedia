1a13ae1a809b1754deb8f91385edd5f4ad39a982
Kaksi Koivua <kaksikoivua@proton.me>
2023-03-21 13:59:52 +0900

    Use Article component for article details

---
+++ src/lib/Article.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { marked } from 'marked';
+
+  export let article;
+
+  const renderer = {
+    heading(text, level) {
+      level += 1;
+      return `<h${level}>${text}</h${level}>`;
+    }
+  };
+  marked.use({ renderer });
+</script>
+
+<article>
+  <h1>{article.title}</h1>
+  {@html marked(article.content1)}
+  {#if article.content2}
+    {@html marked(article.content2)}
+  {/if}
+</article>
+
+<p>client-side data fetching with RLS</p>
+<pre>{JSON.stringify(article, null, 2)}</pre>
--- src/routes/(app)/articles/[slug]/+page.svelte
+++ src/routes/(app)/articles/[slug]/+page.svelte
@@ -1,30 +1,13 @@
 <script lang="ts">
-  import { marked } from 'marked';
+  import Article from '$lib/Article.svelte';
 
   import type { PageData } from './$types';
 
   export let data: PageData;
 
   const article = data.article;
-
-  const renderer = {
-    heading(text, level) {
-      level += 1;
-      return `<h${level}>${text}</h${level}>`;
-    }
-  };
-  marked.use({ renderer });
 </script>
 
 <div><a href="/articles">Article list</a></div>
 
-<article>
-  <h1>{article.title}</h1>
-  {@html marked(article.content1)}
-  {#if article.content2}
-    {@html marked(article.content2)}
-  {/if}
-</article>
-
-<p>client-side data fetching with RLS</p>
-<pre>{JSON.stringify(article, null, 2)}</pre>
+<Article {article}/>
